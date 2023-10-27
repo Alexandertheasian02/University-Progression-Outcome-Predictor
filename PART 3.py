f59e094bd49056c6fe2b598333d05dd17a381503
Damz <65998815+Alexandertheasian02@users.noreply.github.com>
2023-10-27 10:22:19 +0530

    Update PART 3.py

--- PART 3.py
+++ PART 3.py
@@ -1,6 +1,5 @@
 # I declare that my work contains no examples of misconduct, such as plagiarism or collusion.
 # Any code taken from other sources is referenced within my code solution.
-# Student ID: w1953268
 # Date: 08/12/2022
 Progress_list = []  # lists
 Progress_Module_Trailer_list = []
